ce04306126f80af36c2e6616b5d89e83374a7da2
kurudrive <kurudrive@gmail.com>
2019-07-14 01:36:05 +0900

    1st commit

---
+++ inc/customize-header-color/customize-header-color.php
@@ -0,0 +1,129 @@
+<?php
+add_action( 'customize_register', 'lightning_customize_register_color' );
+function lightning_customize_register_color( $wp_customize ) {
+
+	$wp_customize->add_setting(
+		'color_header_subtitle', array(
+			'sanitize_callback' => 'sanitize_text_field',
+		)
+	);
+	$wp_customize->add_control(
+		new Custom_Html_Control(
+			$wp_customize, 'color_header_subtitle', array(
+				'label'            => '',
+				'section'          => 'lightning_design',
+				'type'             => 'text',
+				'custom_title_sub' => __( 'Header Color', 'lightning-pro' ),
+				'custom_html'      => '',
+				'priority'         => 601,
+			)
+		)
+	);
+
+	// color
+	$wp_customize->add_setting(
+		'lightning_theme_options[color_header_text]', array(
+			'default'           => '',
+			'type'              => 'option',
+			'capability'        => 'edit_theme_options',
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize, 'color_header_text', array(
+				'label'    => __( 'Header Text Color', 'lightning-pro' ),
+				'section'  => 'lightning_design',
+				'settings' => 'lightning_theme_options[color_header_text]',
+				'priority' => 601,
+			)
+		)
+	);
+
+	// Header Bg
+	$wp_customize->add_setting(
+		'lightning_theme_options[color_header_bg]', array(
+			'default'           => '',
+			'type'              => 'option',
+			'capability'        => 'edit_theme_options',
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize, 'color_header_bg', array(
+				'label'    => __( 'Header Background Color', 'lightning-pro' ),
+				'section'  => 'lightning_design',
+				'settings' => 'lightning_theme_options[color_header_bg]',
+				'priority' => 601,
+			)
+		)
+	);
+}
+
+
+/* 色の出力 */
+/*-------------------------------------------*/
+/*	Print head
+/*-------------------------------------------*/
+add_action( 'wp_head', 'lightning_print_css_header', 3 );
+function lightning_print_css_header() {
+	$options     = get_option( 'lightning_theme_options' );
+	$dynamic_css = '';
+
+	if ( ! empty( $options['color_header_bg'] ) ) {
+		$color_header_bg = esc_html( $options['color_header_bg'] );
+		$dynamic_css    .= '.headerTop,.siteHeader { background-color:' . $color_header_bg . ' ; }';
+
+		if ( function_exists( 'lightning_check_color_mode' ) && lightning_check_color_mode( $color_header_bg ) == 'light' ) {
+
+			// Light Color ///////////////////
+
+			$dynamic_css .= '.headerTop { border-bottom:1px solid rgba(0,0,0,0.1); }';
+
+		} else {
+
+			// Dark Color ///////////////////
+
+			$dynamic_css .= '
+				.headerTop {
+					border-bottom:1px solid rgba(255,255,255,0.2);
+				}
+				.headerTop_contactBtn .btn.btn-primary {
+					color:' . $color_header_bg . ';
+					background-color: #fff;
+					border-color:rgba(255,255,255,0.2);
+				}
+				.headerTop_contactBtn .btn.btn-primary:hover {
+					opacity: 0.9;
+				}';
+			// Mobile Nav Button
+			$dynamic_css .= '.vk-mobile-nav-menu-btn {
+				border-color:rgba(255,255,255,0.8);
+				background-image: url(' . get_template_directory_uri() . '/inc/vk-mobile-nav/images/vk-menu-btn-white.svg);
+			}';
+		}
+	}
+
+	if ( ! empty( $options['color_header_text'] ) ) {
+		$color_header_text = esc_html( $options['color_header_text'] );
+		$dynamic_css      .= '
+			@media (min-width: 992px){
+				.headerTop,
+				.headerTop li a,
+				ul.gMenu>li>a { color: ' . $color_header_text . ' ; }
+			} /* @media (min-width: 992px) */
+			';
+	}
+
+	if ( $dynamic_css ) {
+		// delete before after space
+		$dynamic_css = trim( $dynamic_css );
+		// convert tab and br to space
+		$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
+		// Change multiple spaces to single space
+		$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
+		wp_add_inline_style( 'lightning-design-style', $dynamic_css );
+	}
+
+}
---
+++ lightning-header-color-manager.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name: Lightning Header Color Manager
+ * Version: 0.0.0
+ * Author: Vektor,Inc.
+ * Description:
+ * License: GPL2
+ * Author URI: http://www.vektor-inc.co.jp
+** /
+
+
+/* ---------------------------------------------
+	updater
+--------------------------------------------- */
+
+require 'inc/plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/vektor-inc/lightning-header-color-manager',
+	__FILE__, // Full path to the main plugin file or functions.php.
+	'lightning-header-color-manager'
+);
+$myUpdateChecker->setBranch( 'master' );
+
+/* ---------------------------------------------
+	Check Theme
+--------------------------------------------- */
+
+add_action( 'after_setup_theme', 'lhcm_plugin_active' );
+function lhcm_plugin_active() {
+	// テーマがLightning系じゃなかったら処理を終了
+	if ( ! function_exists( 'lightning_get_theme_name' ) ) {
+		return;
+	}
+}
+
+/* ---------------------------------------------
+	Run Function
+--------------------------------------------- */
+
+add_action( 'plugins_loaded', 'lhcm_skin_loadfunction' );
+function lhcm_skin_loadfunction() {
+	$skin = get_option( 'lightning_design_skin' );
+	if ( $skin == 'origin' || $skin == 'variety' ) {
+		require plugin_dir_path( __FILE__ ) . 'inc/customize-header-color/customize-header-color.php';
+	}
+}
